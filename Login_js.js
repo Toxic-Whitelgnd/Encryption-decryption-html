0d5d2d75598082b91d7ac7c99ecfc9265a65db90
Toxic-Whitelgnd <90614827+Toxic-Whitelgnd@users.noreply.github.com>
2021-10-14 21:07:11 +0530

    Uploaded By Pain

---
+++ Login_js.js
@@ -0,0 +1,28 @@
+
+function signup(){
+         document.location.href="signup_page.html";
+ }
+    
+
+let a = "akatsuki@gmail.com"
+let b = a.split('@')
+let c = b[0]
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+let name1 = ''
+if(name1 != ''){
+        console.log("it is not empty")
+}
+else{
+        console.log("it is empty")
+}
+
+
+
+{/* <script src="https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js"></script> */}
+//    <script src="https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js"></script>
+//    <script src="https://www.gstatic.com/firebasejs/9.1.2/firebase-database.js"></script>
+    
---
+++ signup_js.js
@@ -0,0 +1,18 @@
+
+
+function back_(){
+    document.location.href="index-login.html"
+}
+
+function clear_(){
+   var email1 = document.getElementById("email_field")
+   var password1 = document.getElementById("password_field")
+   var  username1 = document.getElementById("username_field")
+
+   email1.value=''
+   password1.value=''
+   username1.value=''
+
+
+
+}
